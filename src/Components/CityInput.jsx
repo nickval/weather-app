eaa1aea507f33c8d9e72b09cd05082f92f86fe16
Nickolay Ignatyev <nickval@ukr.net>
2018-10-04 01:29:30 +0300

    added css

--- src/Components/CityInput.jsx
+++ src/Components/CityInput.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 export default function CityInput(props) {
     return(
-        <div>
-            <div>City: {props.city}, your IP: {props.ip}</div>
+        <div  className="City">
+            <div>City: {props.city}</div>
+            <div>Your IP: {props.ip}</div>
+            <br />
             <input value={props.city} onChange={props.onCityChange} type="text" name="name" />
             <button onClick={props.fetchData}>Get weather</button>
+            <p>&nbsp;</p>
         </div>
     );
 }
